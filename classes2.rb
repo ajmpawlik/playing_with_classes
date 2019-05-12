59f526bbc2e6f3e4fc08bd009e6a3fb25b93c5f0
Aleksandra <ajmpawlik@gmail.com>
2019-05-12 23:05:30 +0100

    commiting two first excercises

---
+++ classes2.rb
@@ -0,0 +1,12 @@
+one_string = String.new("1")
+puts one_string
+second_string = String.new("2")
+puts second_string
+one_string = one_string + second_string
+p one_string
+
+sum_of_intigers = 1 + 2
+puts sum_of_intigers
+
+sum_of_i_and_s = "1" + 2
+puts sum_of_i_and_s
